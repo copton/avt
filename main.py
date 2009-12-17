a397b48275ba249fb175f18d024ea8be587cf5f8
Alexander Bernauer <alex@copton.net>
2009-12-17 15:28:07 +0100

    starting workflows with common main script

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
+#! /usr/bin/python
 import guppy
-
+import sys
+import os.path
 from config import config
+
+workflow = os.path.basename(sys.argv[0])
 
 component_selection = {
     "database" : guppy.Component("database.elixir.Elixir"),
     "ui" : guppy.Component("ui.console.UI"),
-    "workflow" : guppy.Component("workflow.lookup.Workflow"),
+    "workflow" : guppy.Component("workflow.%s.Workflow" % workflow),
 }
 
 config.update(component_selection)
--- workflow/lookup/__init__.py
+++ workflow/lookup/__init__.py
@@ -3,3 +3,5 @@ import workflow
 class Workflow(workflow.Workflow):
     def _run(self):
         self.ui.textoutput("here we go")
+
+# words = Word.query.filter(Word.contents.like(u"%%%s" % sys.argv[1])).all()
