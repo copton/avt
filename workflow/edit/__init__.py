78a81ac86d6501f947917bb3fdef60165c8b60b7
Alexander Bernauer <alex@copton.net>
2010-06-06 17:21:18 +0200

    improved edit workflow

--- workflow/edit/__init__.py
+++ workflow/edit/__init__.py
@@ -6,6 +6,18 @@ class Workflow(workflow.Workflow):
     """edit local database"""
 
     def _run(self):
+        def edit(words):
+            idx, word = self._selectWord(words)
+            if word == None:
+                return 
+
+            select = self.ui.play(Menu("what do you want to do?", [
+                        ("edit", "e", self.ui.edit),
+                        ("delete", "d", self.db.remove)
+                    ], quit=True, default="e"))
+            if select != Menu.quit:
+                select(word)
+
         def add():
             contents = self.ui.textinput("enter new word (leave blank to quit)")
             if contents == "":
@@ -14,29 +26,26 @@ class Workflow(workflow.Workflow):
             self.ui.edit(word)
             self.db.add(word)
 
-        def edit():
-            pattern = self.ui.textinput("which word do you want to edit?")
+        def listWords():
+            def compare(lhs, rhs):
+                return lhs.contents < rhs.contents 
+            words = self.db.sort(compare)
+            edit(words)
+
+        def search():
+            pattern = self.ui.textinput("enter pattern")
             matches = self.db.search(pattern)
             if matches == []:
                 self.ui.textoutput("no matches found.")
                 return
-
-            idx, word = self._selectWord(matches)
-            if word == None:
-                return
-                
-            select = self.ui.play(Menu("what do you want to do?", [
-                        ("edit", "e", self.ui.edit),
-                        ("delete", "d", self.db.remove)
-                    ], quit=True, default="e"))
-            if select != Menu.quit:
-                select(word)
-
+            edit(matches)
+            
         while True:
             select = self.ui.play(
                 Menu("what do you want to do?", [
                     ("add new word", "n", add),
-                    ("edit an existing word", "e", edit),
+                    ("search a word", "s", search),
+                    ("list all words", "l", listWords),
                         ], quit=True, default="n"))
 
             if select == Menu.quit:
