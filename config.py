7eb6f4c8a86202ffacb7e23876fa935e53131b20
Alexander Bernauer <alex@copton.net>
2010-06-06 13:12:24 +0200

    race condition

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 config = {
     "database.connection_string" : "/%s/.voctrain+/database.pickle" % os.environ["HOME"],
-    "dictionary.trans_de_en.dict_file" : "/usr/share/trans/de-en",
+    "dictionary.dict_file" : "/usr/share/trans/de-en",
 }
--- dictionary/trans_de_en/__init__.py
+++ dictionary/trans_de_en/__init__.py
@@ -1,15 +1,47 @@
 import dictionary
 from database import Word
 import re
+from threading import Thread
 from guppy import *
 
 class Dictionary(dictionary.Dictionary):
-    dict_file = RequiredFeature("dictionary.trans_de_en.dict_file", isInstanceOf(str))
+    def __init__(self):
+        self.source = Source()
 
     def search(self, pattern):
+        entries = self.source.getEntries()
         rep = re.compile(pattern)
-        f = open(self.dict_file)
+        
         res = []
+        for entry in entries:
+            for word in entry.en:
+                mo = re.search(word)
+                if mo:
+                    res.append(Word(word, "|".join(entry.de)))
+        return res
+
+class Entry(object):
+    __slots__ = ["de", "en"]
+    def __init__(self, de, en):
+        self.de = de
+        self.en = en
+
+
+class Source(Thread):
+    dict_file = RequiredFeature("dictionary.dict_file", isInstanceOf(str))
+
+    def __init__(self):
+        Thread.__init__(self)
+        self.entries = []
+        self.daemon = True
+        self.start()
+
+    def getEntries(self):
+        self.join()
+        return self.entries 
+
+    def run(self):
+        f = open(self.dict_file)
         for line in f.readlines():
             if line.startswith('#'):
                 continue
@@ -19,11 +51,6 @@ class Dictionary(dictionary.Dictionary):
                 continue
             de_parts = map(str.strip, de.split('|'))
             en_parts = map(str.strip, en.split('|'))
-            for de, en in zip(de_parts, en_parts):
-                for synonym in en.split(';'):
-                    mo = rep.search(synonym)
-                    if mo:
-                        res.append(Word(synonym, de))
+            self.entries.append(Entry(de_parts, en_parts))
 
         f.close()
-        return res
